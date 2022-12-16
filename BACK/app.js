d06ea1e6262eb01aff54062000ebc95cbda2dac3
MDJSMD <112764503+MDJSMD@users.noreply.github.com>
2022-12-16 20:20:34 +0100

    Version finale

--- BACK/app.js
+++ BACK/app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const mongoose = require('mongoose');
+require("dotenv").config();
+
+
+
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require('./routes/user');
+const path = require("path");
+
 
 const app = express();
 
 
-mongoose.connect('mongodb+srv://USERAD:useraA78@cluster0.jcgeyau.mongodb.net/?retryWrites=true&w=majority' ,
-{ useNewUrlParser: true,
- useUnifiedTopology: true })
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+  
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.LOGIN_DB,
+    { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+    })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+
+app.use("/api/sauces", sauceRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 module.exports = app;
